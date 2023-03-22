848410d52847e875f3eaf2676a0edd2acc4868d5
Mario Jaramillo <mariojaramillo15@gmail.com>
2023-03-21 20:15:29 -0500

    Creacion de repositorio

---
+++ TareasMVC/Entidades/Tarea.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace TareasMVC.Entidades
+{
+    public class Tarea
+    {
+        public int Id { get; set; }
+        [StringLength(250)]
+        [Required] 
+       
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+
+        public int Orden { get; set;}
+
+        public DateTime FechaCreacion { get; set; }
+        public List<Paso> Paso { get; set; }
+        public List<ArchivoAdjunto> ArchivosAdjuntos { get; set; }
+    }
+}
---
+++ TareasMVC/Models/RegistroViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Principal;
+
+namespace TareasMVC.Models
+{
+    public class RegistroViewModel
+    {
+        [Required(ErrorMessage ="El campo {0} es requerido")]
+        [EmailAddress(ErrorMessage ="El campo de ser un correo electronico valido")]
+        public string Email { get; set; }
+        [Required(ErrorMessage ="El campo {0} es requerido")]
+        [DataType(DataType.Password)]
+        public int MyProperty { get; set; }
+        public string Password { get; set; }
+    }
+}
